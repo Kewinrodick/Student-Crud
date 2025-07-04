b9c296f3b5fea76099253d46820d147ea50fcf6f
Kewinrodick <rodickkewin@gmail.com>
2025-07-04 22:13:12 +0530

    feat:structure is created

---
+++ student/src/main/java/com/example/student/repository/StudentRepository.java
@@ -0,0 +1,7 @@
+package com.example.student.repository;
+
+import com.example.student.entity.Student;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface StudentRepository extends MongoRepository<Student, String> {
+}
