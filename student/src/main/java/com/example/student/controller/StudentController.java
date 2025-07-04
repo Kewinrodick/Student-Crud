d9336a7b9ced92fd4f703ef123e47a605bbdab4b
Kewinrodick <rodickkewin@gmail.com>
2025-07-04 23:25:37 +0530

    feat:addStudent endpoint created

--- student/src/main/java/com/example/student/controller/StudentController.java
+++ student/src/main/java/com/example/student/controller/StudentController.java
@@ -1,4 +1,17 @@
 package com.example.student.controller;
 
+import com.example.student.entity.Student;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/students")
 public class StudentController {
+
+    @PostMapping
+    public Student create(@RequestBody Student student) {
+
+    }
 }
--- student/src/main/java/com/example/student/repository/StudentRepository.java
+++ student/src/main/java/com/example/student/repository/StudentRepository.java
@@ -2,6 +2,8 @@ package com.example.student.repository;
 
 import com.example.student.entity.Student;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StudentRepository extends MongoRepository<Student, String> {
 }
